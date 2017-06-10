d1977d721fbd26c0f8345bfdb5cdd1ef6d21e1e8
Benjamin <begroh@umich.edu>
2017-06-10 19:46:47 -0400

    Added main loop logic
    Added function declarations for checking if a post has been seen already

--- xpostbot.py
+++ xpostbot.py
@@ -1,10 +1,13 @@
 import praw
 import config
+from time import sleep
+
+import sqlite3
 
 import pprint
 
-orig_sub = ''
-xpost_sub = ''
+orig_sub = config.orig_sub
+xpost_sub =  config.xpost_sub
 
 def bot_login():
     r = praw.Reddit(username = config.username,
@@ -14,23 +17,37 @@ def bot_login():
                     user_agent = "test script for xpostbot")
     return r
 
+# TODO Only do posts from past [time period]
 def run(r):
-    for log in r.subreddit(orig_sub).mod.log(action='removelink'):
-        link_id = log.target_fullname[3:]
-        title = log.target_title
-        body = log.target_body
-        r.subreddit(xpost_sub).submit(title, body)
+    while True:
+        for log in r.subreddit(orig_sub).mod.log(action='removelink'):
+            post_id = log.target_fullname
+            title = log.target_title
+            body = log.target_body
 
-# TODO Finds new deletions in the mod log
-def parse_mod_log():
-    pass
+            # If the item has already been checked, we've gone through
+            # all the new log entries
+            if is_present(post_id, title):
+                break
 
-# TODO 
-def crosspost():
-    pass
+            pprint.pprint(vars(log))
+
+            add_to_db(post_id, title)
+
+            # r.subreddit(xpost_sub).submit(title, body)
+            #sleep(60) # Don't want it spamming so only do this once a minute
+
+        sleep(3600) # go through the log once every hour
 
 # TODO Finds mod comment with stated reason for removal
 def get_removal_reason():
+    pass
+
+# TODO
+def add_to_db(post_id, title):
+    pass
+
+def is_present(post_id, title):
     pass
 
 r = bot_login()
