635aab2a112adb9b3665e1ff2a4b92894996917e
Benjamin <begroh@umich.edu>
2017-06-10 21:29:13 -0400

    Added database stuff, can check what's been seen already

--- xpostbot.py
+++ xpostbot.py
@@ -1,6 +1,7 @@
 import praw
 import config
-from time import sleep
+import time
+from datetime import datetime
 
 import sqlite3
 
@@ -8,6 +9,7 @@ import pprint
 
 orig_sub = config.orig_sub
 xpost_sub =  config.xpost_sub
+max_time = config.max_time
 
 def bot_login():
     r = praw.Reddit(username = config.username,
@@ -20,35 +22,63 @@ def bot_login():
 # TODO Only do posts from past [time period]
 def run(r):
     while True:
+        conn = sqlite3.connect('xposttest.db')
+        c = conn.cursor()
+
         for log in r.subreddit(orig_sub).mod.log(action='removelink'):
             post_id = log.target_fullname
             title = log.target_title
             body = log.target_body
+            
+            # If the post is older than a specified amount of time, ignore it
+            # Also means you're done
+            if post_time_out_of_range(log):
+                break
 
             # If the item has already been checked, we've gone through
             # all the new log entries
-            if is_present(post_id, title):
+            if is_present(conn, c, post_id):
                 break
 
-            pprint.pprint(vars(log))
-
-            add_to_db(post_id, title)
+            add_to_db(conn, c, post_id, title)
 
             # r.subreddit(xpost_sub).submit(title, body)
-            #sleep(60) # Don't want it spamming so only do this once a minute
+            #time.sleep(60) # Don't want it spamming so only do this once a minute
 
-        sleep(3600) # go through the log once every hour
+        conn.close()
+        print ("Exiting...")
+        return
+        #time.sleep(3600) # go through the log once every hour
 
 # TODO Finds mod comment with stated reason for removal
 def get_removal_reason():
     pass
 
-# TODO
-def add_to_db(post_id, title):
-    pass
+# Check to see whether a post is in the database
+def is_present(conn, c, post_id):
+    c.execute("SELECT * FROM seen WHERE id=?", (post_id,))
+    if c.fetchone() is not None:
+        print ("Thread has already been processed")
+        return True
+    print ("Thread not in database")
+    return False
 
-def is_present(post_id, title):
-    pass
+# Add a new post to the DB
+def add_to_db(conn, c, post_id, title):
+    try:
+        c.execute("INSERT INTO seen (id, title) VALUES (?, ?)", (post_id, title))
+        conn.commit()
+        print ("Adding thread", title, "to database")
+    except sqlite3.Error as e:
+        print ("An error occured:", e.args[0])
+
+    c.execute("SELECT * FROM seen")
+    print (c.fetchall())
+
+def post_time_out_of_range(log):
+    if (time.time() - log.created_utc > max_time):
+        print ("Thread too old")
+        return True
 
 r = bot_login()
 run(r)
