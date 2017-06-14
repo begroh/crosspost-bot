4a2ed30a6576aa879f08382def5be877de274386
Benjamin <begroh@umich.edu>
2017-06-14 17:26:28 -0400

    Changes config to heroku environment variables

--- xpostbot.py
+++ xpostbot.py
@@ -1,31 +1,26 @@
+import os
 import praw
-import config
 import time
 from datetime import datetime
 
 import pprint
 
-orig_sub = config.orig_sub
-xpost_sub =  config.xpost_sub
-max_time = config.max_time
+orig_sub = os.environ['ORIG_SUB']
+xpost_sub =  os.environ['XPOST_SUB']
+max_time = 600
 
 def bot_login():
-    r = praw.Reddit(username = config.username,
-                    password = config.password,
-                    client_id = config.client_id,
-                    client_secret = config.client_secret,
+    r = praw.Reddit(username = os.environ['REDDIT_USERNAME'],
+                    password = os.environ['REDDIT_PASSWORD'],
+                    client_id = os.environ['REDDIT_ID'],
+                    client_secret = os.environ['REDDIT_SECRET'],
                     user_agent = "A bot for crossposting deleted threads.")
     return r
 
 def run(r):
-    while True:
-        scan_removed_posts(r)
+    scan_removed_posts(r)
 
-        # Can add more functionality here as necessary
-
-        print ("Exiting...")
-        return
-        #time.sleep(1800) # go through the log once every half hour
+    # Can add more functionality here as necessary
 
 # TODO Only do posts from past [time period]
 def scan_removed_posts(r):
@@ -49,11 +44,6 @@ def scan_removed_posts(r):
 # TODO Finds mod comment with stated reason for removal
 def get_removal_reason():
     pass
-
-# TODO
-# Check to see whether a post has already been crossposted
-def is_present(title, post_id):
-    return True
 
 def post_time_out_of_range(log):
     if (time.time() - log.created_utc > max_time):
